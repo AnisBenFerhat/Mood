125ccd133c42f9a9812f981961e2413bc5e9b3fc
AnisBenFerhat <anisbenferhat@gmail.com>
2023-11-30 16:56:47 +0100

    feat(Journal): Implement entry editing functionality
    - Add updateEntry method to interact with the API for entry updates
    - Modify Prisma schema to ensure unique user entry combinations
    - Implement an editor component using textarea for entry content
    - Create PATCH method for updating entry content
    - Integrate autosave functionality using react-autosave library
    - Implemente revalidation of the '/journal' path after entry creation
    or update
    - Added EntryPage component to render entry editor based on ID

--- utils/api.ts
+++ utils/api.ts
@@ -2,6 +2,19 @@ const createURL = (path) => {
   return window.location.origin + path
 }
 
+export const updateEntry = async (id, content) => {
+  const response = await fetch(
+    new Request(createURL(`/api/journal/${id}`), {
+      method: 'PATCH',
+      body: JSON.stringify({ content }),
+    }),
+  )
+  if (response.ok) {
+    const data = await response.json()
+    return data.data
+  }
+}
+
 export const createNewEntry = async () => {
   const response = await fetch(
     new Request(createURL('/api/journal'), {
