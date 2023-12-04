890abd7a65d1d3a7873543ebd512bf42046969f8
AnisBenFerhat <anisbenferhat@gmail.com>
2023-12-04 18:53:14 +0100

    Feat(Journal): Implement analysis integration for journal entries
    - Prompt Template for OpenAI
    - Save Analysis in Database
    - Connect Sidebar UI with Analysis
    - Add Analysis to Entry Update
    - Revalidate Client State
    - Include Navigation Links

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,24 @@
 import { UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
 import { ReactNode } from 'react'
+
+const links = [
+  { href: '/', label: 'Home' },
+  { href: '/journal', label: 'Journal' },
+]
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className='h-screen w-screen relative bg-gradient-to-br from-zinc-900 to-slate-800 text-white/90'>
       <aside className='absolute w-[200px] top-0 left-0 h-full border-r border-white/50 '>
-        Mood
+        <div>Mood</div>
+        <ul>
+          {links.map((link) => (
+            <li key={link.href} className='px-2 py-6 text-xl'>
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
       </aside>
       <div className='ml-[200px] h-full]'>
         <header className='h-[60px] border-b  border-white/50'>
--- utils/ai.ts
+++ utils/ai.ts
@@ -1,27 +1,43 @@
-import { OpenAI } from 'langchain/llms/openai'
-import { StructuredOutputParser } from 'langchain/output_parsers'
-import z from 'zod'
+import { analyze } from '@/utils/ai'
+import { getUserByClerkID } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+import { NextResponse } from 'next/server'
 
-const parser = StructuredOutputParser.fromZodSchema(
-  z.object({
-    mood: z.string().describe('the mood of the persn who wrote the journal entry.'),
-    summary: z.string().describe('quick summary of the entire entry.'),
-    negative: z
-      .boolean()
-      .describe('is the journal entry negative? (i.e. does it contain negative emotions?).'),
-    color: z
-      .string()
-      .describe(
-        'a hexadecimal color code that represents the mood of the entry. For example, #B7F06A for green represents peace, quietude.',
-      ),
-  }),
-)
+export const PATCH = async (request: Request, { params }) => {
+  try {
+    const { content } = await request.json()
+    const user = await getUserByClerkID()
 
-export const analyze = async (prompt) => {
-  const model = new OpenAI({
-    temperature: 0,
-    modelName: 'gpt-3.5-turbo',
-  })
-  const result = await model.call(prompt)
-  console.log(result)
+    const updatedEntry = await prisma.journalEntry.update({
+      where: {
+        userId_id: {
+          userId: user.id,
+          id: params.id,
+        },
+      },
+      data: {
+        content,
+      },
+    })
+
+    const analysis = await analyze(updatedEntry.content)
+
+    const updatedAnalysis = await prisma.analysis.upsert({
+      where: {
+        entryId: updatedEntry.id,
+      },
+      create: {
+        entryId: updatedEntry.id,
+        ...analysis,
+      },
+      update: {
+        ...analysis,
+      },
+    })
+
+    return NextResponse.json({ data: updatedEntry })
+  } catch (error) {
+    console.error('Error in PATCH request:', error)
+    return NextResponse.error(error.message || 'An error occurred')
+  }
 }
